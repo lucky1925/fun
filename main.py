293b2c116266ac87ea9bc74891ba2765875f4b2b
lucky1925 <115342000+lucky1925@users.noreply.github.com>
2022-11-07 17:15:14 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def get_weather(region):
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response["now"]["windDir"]
-    return weather, temp, wind_dir,tempa,tempn
+    return weather, temp, wind_dir
  
  
 def get_birthday(birthday, year, today):
